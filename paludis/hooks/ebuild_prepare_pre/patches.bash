7000722f0fb639ead17f0ff25de3665ca3f6aab8
elysian <tsujianii@tuta.io>
2022-05-18 16:45:05 +0800

    add paludis config

---
+++ paludis/hooks/ebuild_prepare_pre/patches.bash
@@ -0,0 +1,18 @@
+# ty
+# https://github.com/Keruspe/paludis-config/blob/exherbo/hooks/ebuild_prepare_pre/patches.bash
+# vim: set sw=4 sts=4 et :
+
+(
+    edo pushd "${MESON_SOURCE:-${WORK}}"
+    patchdir="/etc/paludis/autopatch/${CATEGORY}/${PN}"
+    if [[ -d "${patchdir}" ]] ; then
+        einfo "Applying user patches"
+        for p in "${patchdir}"/*.patch ; do
+            [[ -f "${p}" ]] || continue
+            einfo "Applying $(basename ${p} )"
+            edo cat "${p}" | patch -s -f -p1
+        done
+        einfo "Done"
+    fi
+    edo popd
+)
